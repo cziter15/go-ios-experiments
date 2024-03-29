f0eeaee674052be449d181e81740572002a74d37
Krzysiek S <chris.streh@gmail.com>
2024-03-29 13:12:24 +0100

    Update connection.go

--- ios/dtx_codec/connection.go
+++ ios/dtx_codec/connection.go
@@ -194,7 +194,7 @@ func (dtxConn *Connection) ForChannelRequest(messageDispatcher Dispatcher) *Chan
 // If someone wants to do that and bring some clarity, please go ahead :-)
 // This channel seems to always be there without explicitly requesting it and sometimes it is used.
 func (dtxConn *Connection) AddDefaultChannelReceiver(messageDispatcher Dispatcher) *Channel {
-	channel := &Channel{channelCode: -1, channelName: "c -1/ 4294967295 receiver channel ", messageIdentifier: 1, connection: dtxConn, messageDispatcher: messageDispatcher, responseWaiters: map[int]chan Message{}, defragmenters: map[int]*FragmentDecoder{}, timeout: 5 * time.Second}
+	channel := &Channel{channelCode: 4294967295, channelName: "c -1/ 4294967295 receiver channel ", messageIdentifier: 1, connection: dtxConn, messageDispatcher: messageDispatcher, responseWaiters: map[int]chan Message{}, defragmenters: map[int]*FragmentDecoder{}, timeout: 5 * time.Second}
 	dtxConn.activeChannels.Store(uint32(math.MaxUint32), channel)
 	return channel
 }
